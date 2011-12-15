6106400b0225e0722eb205ddab347fa3d1365d67
glopes <goncaloclopes@gmail.com>
2011-12-15 13:21:35 +0000

    Added support for cvNot.

--- OpenCV.Net/Core.cs
+++ OpenCV.Net/Core.cs
@@ -58,6 +58,9 @@ namespace OpenCV.Net
         public static extern void cvAnd(CvArr src1, CvArr src2, CvArr dst, CvArr mask);
 
         [DllImport(libName)]
+        public static extern void cvNot(CvArr src, CvArr dst);
+
+        [DllImport(libName)]
         public static extern void cvInRangeS(CvArr src, CvScalar lower, CvScalar upper, CvArr dst);
 
         public static CvScalar cvAvg(CvArr arr)
