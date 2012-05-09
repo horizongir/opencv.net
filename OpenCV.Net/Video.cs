306dceb3b174415566a6e4509f98ef9fe9786219
glopes <glopesdev@users.noreply.github.com>
2012-05-09 11:06:51 +0100

    Fixed cvCalcOpticalFlowPyrLK method to correctly output feature array.

--- OpenCV.Net/Video.cs
+++ OpenCV.Net/Video.cs
@@ -17,7 +17,7 @@ namespace OpenCV.Net
         public static extern void cvCalcOpticalFlowHS(CvArr prev, CvArr curr, int usePrevious, CvArr velx, CvArr vely, double lambda, CvTermCriteria criteria);
 
         [DllImport(libName, CallingConvention = CallingConvention.Cdecl)]
-        public static extern void cvCalcOpticalFlowPyrLK(CvArr prev, CvArr curr, CvArr prevPyr, CvArr currPyr, CvPoint2D32f[] prevFeatures, CvPoint2D32f[] currFeatures, int count, CvSize winSize, int level, byte[] status, float[] track_error, CvTermCriteria criteria, LKFlowFlags flags);
+        public static extern void cvCalcOpticalFlowPyrLK(CvArr prev, CvArr curr, CvArr prevPyr, CvArr currPyr, CvPoint2D32f[] prevFeatures, [Out]CvPoint2D32f[] currFeatures, int count, CvSize winSize, int level, byte[] status, float[] track_error, CvTermCriteria criteria, LKFlowFlags flags);
 
         [DllImport(libName, CallingConvention = CallingConvention.Cdecl)]
         public static extern void cvUpdateMotionHistory(CvArr silhouette, CvArr mhi, double timestamp, double duration);
