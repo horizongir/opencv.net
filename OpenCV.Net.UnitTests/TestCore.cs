c10eee2c8d774f26f5bd0077adc6737e3b822eec
glopes <glopesdev@users.noreply.github.com>
2013-05-30 19:31:10 +0100

    Added unit tests project with basic matrix tests.

---
+++ OpenCV.Net.UnitTests/TestCore.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OpenCV.Net.UnitTests
+{
+    [TestClass]
+    public class TestCore
+    {
+        [TestMethod]
+        public void TestAlloc()
+        {
+            var size = new UIntPtr(1024);
+            var ptr = cv.Alloc(size);
+            Assert.AreNotEqual(ptr, IntPtr.Zero);
+        }
+
+        [TestMethod]
+        public void TestFree()
+        {
+            var size = new UIntPtr(1024);
+            var ptr = cv.Alloc(size);
+            Assert.AreNotEqual(ptr, IntPtr.Zero);
+            cv.Free(ref ptr);
+            Assert.AreEqual(ptr, IntPtr.Zero);
+        }
+    }
+}
---
+++ OpenCV.Net.UnitTests/TestCvMat.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OpenCV.Net.UnitTests
+{
+    [TestClass]
+    public class TestCvMat
+    {
+        const int Cols = 320;
+        const int Rows = 240;
+        const int Channels = 1;
+        const CvMatDepth Depth = CvMatDepth.F32;
+
+        CvMat mat;
+
+        [TestInitialize]
+        public void InitializeCvMat()
+        {
+            mat = new CvMat(Rows, Cols, Depth, Channels);
+            Assert.AreEqual(mat.IsInvalid, false);
+        }
+
+        [TestCleanup]
+        public void CleanupCvMat()
+        {
+            mat.Close();
+            Assert.AreEqual(mat.IsClosed, true);
+        }
+
+        [TestMethod]
+        public void TestCvMatRows()
+        {
+            Assert.AreEqual(mat.Rows, Rows);
+        }
+
+        [TestMethod]
+        public void TestCvMatCols()
+        {
+            Assert.AreEqual(mat.Cols, Cols);
+        }
+
+        [TestMethod]
+        public void TestCvMatDepth()
+        {
+            Assert.AreEqual(mat.Depth, Depth);
+        }
+
+        [TestMethod]
+        public void TestCvMatChannels()
+        {
+            Assert.AreEqual(mat.NumChannels, Channels);
+        }
+
+        [TestMethod]
+        public void TestCvMatStep()
+        {
+            Assert.AreEqual(mat.Step, mat.Cols * mat.ElementSize);
+        }
+    }
+}
