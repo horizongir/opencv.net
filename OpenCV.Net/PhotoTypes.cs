460f1ee69d49465c9247d5d8894dd5b510f612a6
glopes <glopesdev@users.noreply.github.com>
2013-08-01 19:31:33 +0100

    Completed support for photo module.

---
+++ OpenCV.Net/PhotoTypes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenCV.Net
+{
+    /// <summary>
+    /// Specifies available inpainting methods.
+    /// </summary>
+    public enum InpaintMethod : int
+    {
+        /// <summary>
+        /// Specifies a Navier-Stokes based method.
+        /// </summary>
+        NavierStokes = 0,
+
+        /// <summary>
+        /// Specifies the method by Alexandru Telea.
+        /// </summary>
+        Telea = 1
+    }
+}
