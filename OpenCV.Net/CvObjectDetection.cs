4c2db5f091deaaa9d2350c39392f66f9e2667b0e
glopes <glopes@localhost>
2013-08-01 20:35:13 +0100

    Added support for latent svm detector and missing objdetect structures.

---
+++ OpenCV.Net/CvObjectDetection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace OpenCV.Net
+{
+    /// <summary>
+    /// Represents a structure that contains the bounding box and confidence level for a detected object.
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential)]
+    public struct CvObjectDetection
+    {
+        /// <summary>
+        /// The bounding box for the detected object.
+        /// </summary>
+        public CvRect Rect;
+
+        /// <summary>
+        /// The detection confidence level.
+        /// </summary>
+        public float Score;
+    }
+}
