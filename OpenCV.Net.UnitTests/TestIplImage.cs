7ea9fda8ada3158174e31a79fa8fb6663e3e4c7a
glopes <glopesdev@users.noreply.github.com>
2013-08-02 09:49:18 +0100

    Added unit tests for image header, clone and channel of interest.

--- OpenCV.Net.UnitTests/TestIplImage.cs
+++ OpenCV.Net.UnitTests/TestIplImage.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Runtime.InteropServices;
 
 namespace OpenCV.Net.UnitTests
 {
@@ -39,6 +40,51 @@ namespace OpenCV.Net.UnitTests
             }
 
             return image = new IplImage(new CvSize(dim1, dim0), iplDepth, channels);
+        }
+
+        [TestMethod]
+        public void TestImageHeader()
+        {
+            var data = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var dataHandle = GCHandle.Alloc(data, GCHandleType.Pinned);
+            using (var image = new IplImage(new CvSize(3, 3), IplDepth.S32, 1, dataHandle.AddrOfPinnedObject()))
+            {
+                Assert.AreEqual(data[7], image[2, 1].Val0);
+            }
+            dataHandle.Free();
+        }
+
+        [TestMethod]
+        public void TestClone()
+        {
+            using (var image = new IplImage(new CvSize(3, 3), IplDepth.F32, 1))
+            {
+                image.SetZero();
+                image[1, 1] = CvScalar.All(3);
+                Assert.AreEqual(3, image[1, 1].Val0);
+                using (var clone = image.Clone())
+                {
+                    Assert.AreEqual(image.Size, clone.Size);
+                    Assert.AreEqual(image[1, 1], clone[1, 1]);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void TestImageCoi()
+        {
+            using (var image = new IplImage(new CvSize(3, 3), IplDepth.F32, 3))
+            {
+                image.SetZero();
+                image[1, 1] = new CvScalar(0, 1, 0, 0);
+                using (var mask = new IplImage(image.Size, image.Depth, 1))
+                {
+                    image.ChannelOfInterest = 2;
+                    cv.Copy(image, mask);
+                    image.ChannelOfInterest = 0;
+                    Assert.AreEqual(image[1, 1].Val1, mask[1, 1].Val0);
+                }
+            }
         }
     }
 }
