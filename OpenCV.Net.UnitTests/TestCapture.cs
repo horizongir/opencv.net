1c39d2f0b970e33e326c593c4af5cfc463f2527a
glopes <glopes@localhost>
2013-08-15 11:45:07 +0100

    Fixed CreateFileCapture method and added corresponding unit tests.

---
+++ OpenCV.Net.UnitTests/TestCapture.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OpenCV.Net.UnitTests
+{
+    [TestClass]
+    public class TestCapture
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TestFileCaptureNullPath()
+        {
+            Capture.CreateFileCapture(null);
+        }
+
+        [TestMethod]
+        public void TestFileCaptureInvalidPath()
+        {
+            var capture = Capture.CreateFileCapture(string.Empty);
+            Assert.AreEqual(null, capture);
+        }
+    }
+}
