d319aaf4d5dbcefcebcab256026e68a18542f269
glopes <glopesdev@users.noreply.github.com>
2014-01-12 11:18:34 +0000

    Updated Arr unit testing to expect exceptions on incompatible derived types.

--- OpenCV.Net.UnitTests/MatNDTests.cs
+++ OpenCV.Net.UnitTests/MatNDTests.cs
@@ -12,5 +12,68 @@ namespace OpenCV.Net.UnitTests
         {
             return mat = new MatND(new[] { dim0, dim1 }, depth, channels);
         }
+
+        [TestMethod]
+        [ExpectedException(typeof(CVException))]
+        public new void GetSize_ReturnsArrWidthAndHeight()
+        {
+            base.GetSize_ReturnsArrWidthAndHeight();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(CVException))]
+        public new void GetSubRect_ReturnsMatWithSubRectDimensions()
+        {
+            base.GetSubRect_ReturnsMatWithSubRectDimensions();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(CVException))]
+        public new void GetRow_ReturnsMatVectorWithArrWidthLength()
+        {
+            base.GetRow_ReturnsMatVectorWithArrWidthLength();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(CVException))]
+        public new void GetRows_ReturnsMatWithHeightEqualToSelectedRowCount()
+        {
+            base.GetRows_ReturnsMatWithHeightEqualToSelectedRowCount();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(CVException))]
+        public new void GetCol_ReturnsMatVectorWithArrHeightLength()
+        {
+            base.GetCol_ReturnsMatVectorWithArrHeightLength();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(CVException))]
+        public new void GetCols_ReturnsMatWithWidthEqualToSelectedRowCount()
+        {
+            base.GetCols_ReturnsMatWithWidthEqualToSelectedRowCount();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(CVException))]
+        public new void GetDiag_ReturnsMatWithArrHeight()
+        {
+            base.GetDiag_ReturnsMatWithArrHeight();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(CVException))]
+        public new void GetImage_ReturnsIplImageWrapperTo2DDenseArr()
+        {
+            base.GetImage_ReturnsIplImageWrapperTo2DDenseArr();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(CVException))]
+        public new void SampleLine_ReturnsBufferWithLineElementCountLength()
+        {
+            base.SampleLine_ReturnsBufferWithLineElementCountLength();
+        }
     }
 }
--- OpenCV.Net.UnitTests/SparseMatTests.cs
+++ OpenCV.Net.UnitTests/SparseMatTests.cs
@@ -14,6 +14,164 @@ namespace OpenCV.Net.UnitTests
         }
 
         [TestMethod]
+        [ExpectedException(typeof(CVException))]
+        public new void GetSize_ReturnsArrWidthAndHeight()
+        {
+            base.GetSize_ReturnsArrWidthAndHeight();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(CVException))]
+        public new void GetSubRect_ReturnsMatWithSubRectDimensions()
+        {
+            base.GetSubRect_ReturnsMatWithSubRectDimensions();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(CVException))]
+        public new void GetRow_ReturnsMatVectorWithArrWidthLength()
+        {
+            base.GetRow_ReturnsMatVectorWithArrWidthLength();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(CVException))]
+        public new void GetRows_ReturnsMatWithHeightEqualToSelectedRowCount()
+        {
+            base.GetRows_ReturnsMatWithHeightEqualToSelectedRowCount();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(CVException))]
+        public new void GetCol_ReturnsMatVectorWithArrHeightLength()
+        {
+            base.GetCol_ReturnsMatVectorWithArrHeightLength();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(CVException))]
+        public new void GetCols_ReturnsMatWithWidthEqualToSelectedRowCount()
+        {
+            base.GetCols_ReturnsMatWithWidthEqualToSelectedRowCount();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(CVException))]
+        public new void GetDiag_ReturnsMatWithArrHeight()
+        {
+            base.GetDiag_ReturnsMatWithArrHeight();
+        }
+
+        [TestMethod]
+        public new void Ptr3D_ReturnsNonZeroPointer()
+        {
+            base.Ptr3D_ReturnsNonZeroPointer();
+        }
+
+        [TestMethod]
+        public new void Get3D_ReturnsCorrectElementValue()
+        {
+            base.Get3D_ReturnsCorrectElementValue();
+        }
+
+        [TestMethod]
+        public new void GetReal3D_ReturnsCorrectElementValue()
+        {
+            base.GetReal3D_ReturnsCorrectElementValue();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(CVException))]
+        public new void GetMat_ReturnsMatWrapperTo2DDenseArr()
+        {
+            base.GetMat_ReturnsMatWrapperTo2DDenseArr();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(CVException))]
+        public new void GetImage_ReturnsIplImageWrapperTo2DDenseArr()
+        {
+            base.GetImage_ReturnsIplImageWrapperTo2DDenseArr();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(CVException))]
+        public new void Reshape_ReturnsReshapedMatFor2DDenseArr()
+        {
+            base.Reshape_ReturnsReshapedMatFor2DDenseArr();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(CVException))]
+        public new void GetRawData_ReturnsNonZeroPointer()
+        {
+            base.GetRawData_ReturnsNonZeroPointer();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(CVException))]
+        public new void Set_ArrElementsHaveTargetValue()
+        {
+            base.Set_ArrElementsHaveTargetValue();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(CVException))]
+        public new void CheckRange_ValidRangeElements_ReturnsTrue()
+        {
+            base.CheckRange_ValidRangeElements_ReturnsTrue();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(CVException))]
+        public new void Repeat_DestinationArrIsTiledWithSourceArr()
+        {
+            base.Repeat_DestinationArrIsTiledWithSourceArr();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(CVException))]
+        public new void Split_DestinationArrPlaneHasCorrectChannelValues()
+        {
+            base.Split_DestinationArrPlaneHasCorrectChannelValues();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(CVException))]
+        public new void Merge_DestinationArrHasCorrectChannelValues()
+        {
+            base.Merge_DestinationArrHasCorrectChannelValues();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(CVException))]
+        public new void MixChannels_TargetArrHasRemappedChannelValues()
+        {
+            base.MixChannels_TargetArrHasRemappedChannelValues();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(CVException))]
+        public new void ConvertScale_DestinationArrHasCorrectlyScaledElements()
+        {
+            base.ConvertScale_DestinationArrHasCorrectlyScaledElements();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(CVException))]
+        public new void ConvertScaleAbs_DestinationArrHasCorrectlyScaledElements()
+        {
+            base.ConvertScaleAbs_DestinationArrHasCorrectlyScaledElements();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(CVException))]
+        public new void SampleLine_ReturnsBufferWithLineElementCountLength()
+        {
+            base.SampleLine_ReturnsBufferWithLineElementCountLength();
+        }
+
+        [TestMethod]
         public void SparseMatIterator_CorrectlyEnumeratesSparseElementNodes()
         {
             using (var mat = new SparseMat(new[] { 3, 3 }, Depth.F32, 2))
