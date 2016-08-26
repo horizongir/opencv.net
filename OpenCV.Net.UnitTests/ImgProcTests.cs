f525adb8e59587927704bf93ac4c1db933158680
markeshov <glopesdev@users.noreply.github.com>
2016-08-26 11:26:51 +0300

    Added ImgProc tests

---
+++ OpenCV.Net.UnitTests/ImgProcTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OpenCV.Net.UnitTests
+{
+    [TestClass]
+    public class ImgProcTests
+    {
+        [TestMethod]
+        public void BoxPoints_Test()
+        {
+            var rotatedRect = new RotatedRect(new Point2f(50,50), new Size2f(30,30), 45);
+            Point2f[] points = new Point2f[4];
+            CV.BoxPoints(rotatedRect, points);
+            Assert.IsTrue(points[0].X != 0 && points[0].Y != 0);
+            Assert.IsTrue(points[1].X != 0 && points[1].Y != 0);
+            Assert.IsTrue(points[2].X != 0 && points[2].Y != 0);
+            Assert.IsTrue(points[3].X != 0 && points[3].Y != 0);
+        }
+    }
+}
