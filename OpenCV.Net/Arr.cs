26454d5faabcb9f7068112291bd2f143a24e8825
glopes <glopesdev@users.noreply.github.com>
2016-11-16 23:04:49 +0000

    Removed unused using directives for .NET Standard 1.2 compatibility.

--- OpenCV.Net/Arr.cs
+++ OpenCV.Net/Arr.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using Microsoft.Win32.SafeHandles;
 using OpenCV.Net.Native;
 
 namespace OpenCV.Net
--- OpenCV.Net/Seq.cs
+++ OpenCV.Net/Seq.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.Win32.SafeHandles;
-using OpenCV.Net.Native;
+﻿using OpenCV.Net.Native;
 using System;
 using System.Collections.Generic;
 using System.Linq;
