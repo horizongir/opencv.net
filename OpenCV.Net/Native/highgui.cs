6bd7c7fc2ab6f1394ee0656241756ad4dc00a263
glopesdev <glopesdev@users.noreply.github.com>
2021-12-27 15:48:15 +0000

    Add separate lib suffix for native bindings

--- OpenCV.Net/Native/highgui.cs
+++ OpenCV.Net/Native/highgui.cs
@@ -8,7 +8,7 @@ namespace OpenCV.Net.Native
 {
     static partial class NativeMethods
     {
-        const string highguiLib = "opencv_highgui248";
+        const string highguiLib = "opencv_highgui" + libSuffix;
 
         #region Basic GUI functions
 
--- OpenCV.Net/Native/photo.cs
+++ OpenCV.Net/Native/photo.cs
@@ -8,7 +8,7 @@ namespace OpenCV.Net.Native
 {
     static partial class NativeMethods
     {
-        const string photoLib = "opencv_photo248";
+        const string photoLib = "opencv_photo" + libSuffix;
 
         [DllImport(photoLib, CallingConvention = CallingConvention.Cdecl)]
         internal static extern void cvInpaint(Arr src, Arr inpaint_mask, Arr dst, double inpaintRange, InpaintMethod flags);
